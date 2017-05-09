35668c1d29b286401e4b0eaffd1451a5cc52bbfe
Amery Han <ahan@coupang.com>
2017-05-09 16:34:21 +0800

    java connect scala

--- scala/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-object HelloWorld {
-    def main(args: Array[String]): Unit = {
-        println("Hello, worldvvvvvvvv!")
-        var s = "cai niao"; println(s);
-    }
-}
---
+++ scala/src/main/scala/yjmyzz/Hello.scala
@@ -0,0 +1,12 @@
+package yjmyzz
+
+/**
+  * Created by ahan on 09/05/2017.
+  */
+class Hello {
+
+  def sayHello(x: String): Unit = {
+    println("hello amery, I am " + x);
+  }
+
+}
