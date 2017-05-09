6535c9bb59ce2205a9b954191e4a25fcc923e34d
Amery Han <ahan@coupang.com>
2017-05-09 14:34:39 +0800

    add scala

--- scala/HelloWorld.scala
+++ scala/HelloWorld.scala
@@ -1,5 +1,6 @@
 object HelloWorld {
     def main(args: Array[String]): Unit = {
-        println("Hello, world!")
+        println("Hello, worldvvvvvvvv!")
+        var s = "cai niao"; println(s);
     }
 }
