90f8508d8367628413da53d563bf0bdc5c87e67a
Amery Han <ahan@coupang.com>
2017-07-11 16:28:23 +0800

    add reactor mode thread

---
+++ ahan/src/main/java/amery/jdk/io/NIOClient.java
@@ -0,0 +1,26 @@
+package amery.jdk.io;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetSocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by ahan on 11/07/2017.
+ */
+public class NIOClient {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SocketChannel socketChannel = SocketChannel.open();
+		InetSocketAddress address = new InetSocketAddress(1234);
+		socketChannel.connect(address);
+		RandomAccessFile file = new RandomAccessFile(
+			NIOClient.class.getClassLoader().getResource("test.txt").getFile(), "rw");
+		FileChannel channel = file.getChannel();
+		channel.transferTo(0, channel.size(), socketChannel);
+		channel.close();
+		file.close();
+		socketChannel.close();
+	}
+}
