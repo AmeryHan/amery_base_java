15447f02fcc724f9bcc16e35cfaccdfc4d26de96
Amery Han <ahan@coupang.com>
2017-08-07 13:45:59 +0800

    [DRAG-3462] add akka

---
+++ ahan/src/main/java/amery/akka/Greeter.java
@@ -0,0 +1,23 @@
+package amery.akka;
+
+import akka.actor.UntypedActor;
+
+/**
+ * Created by ahan on 07/08/2017.
+ */
+public class Greeter  extends UntypedActor {
+
+	public static enum Msg {
+		GREET, DONE;
+	}
+
+	@Override
+	public void onReceive(Object msg) throws InterruptedException {
+		if (msg == Msg.GREET) {
+			System.out.println("Hello World!");
+			Thread.sleep(1000);
+			getSender().tell(Msg.DONE, getSelf());
+		} else
+			unhandled(msg);
+	}
+}
---
+++ ahan/src/main/java/amery/akka/Main.java
@@ -0,0 +1,11 @@
+package amery.akka;
+
+/**
+ * Created by ahan on 07/08/2017.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		akka.Main.main(new String[] { HelloWorld.class.getName() });
+	}
+}
