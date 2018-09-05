38fa7d5baa037f41a4bfdf0a23c9cc3f9d8495d6
Amery Han <ahan@coupang.com>
2018-09-05 14:43:50 +0800

    add some queue tests

---
+++ ahan/src/main/java/amery/jdk/concurrent/ConcurrentLinkedQueueTest.java
@@ -0,0 +1,72 @@
+package amery.jdk.concurrent;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrentLinkedQueueTest {
+
+	private static int count = 2; // 线程个数
+
+	private static CountDownLatch latch = new CountDownLatch(count);
+	private static ConcurrentLinkedQueue<Integer> queue = new ConcurrentLinkedQueue<Integer>();
+
+	public static void main(String[] args) throws InterruptedException {
+
+		long timeStart = System.currentTimeMillis();
+
+		ExecutorService es = Executors.newFixedThreadPool(4);
+
+		ConcurrentLinkedQueueTest.offer();
+
+		for (int i = 0; i < count; i++) {
+
+			es.submit(new Poll());
+
+		}
+
+		latch.await(); //使得主线程(main)阻塞直到latch.countDown()为零才继续执行
+
+		System.out.println("cost time " + (System.currentTimeMillis() - timeStart) + "ms");
+
+		es.shutdown();
+
+	}
+
+	/**
+
+	 * 生产
+	 * add 和offer() 都是加入元素的方法(在ConcurrentLinkedQueue中这俩个方法没有任何区别)
+	 * poll() 和peek() 都是取头元素节点，区别在于前者会删除元素，后者不会。
+
+	 */
+
+	public static void offer() {
+
+		for (int i = 0; i < 100000; i++) {
+
+			queue.offer(i);
+
+		}
+
+	}
+
+	static class Poll implements Runnable {
+
+		public void run() {
+
+			// while (queue.size()>0) {
+
+			while (!queue.isEmpty()) {
+
+				System.out.println(queue.poll());
+
+			}
+
+			latch.countDown();
+
+		}
+
+	}
+}
---
+++ ahan/src/main/java/amery/jdk/concurrent/PriorityBlockingQueueTest.java
@@ -0,0 +1,175 @@
+package amery.jdk.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class PriorityBlockingQueueTest {
+
+	//static Logger logger = LogManager.getLogger(QueueTest.class);
+
+	static Random random = new Random(47);
+
+	public static void main(String args[]) throws InterruptedException
+
+	{
+
+		PriorityBlockingQueue<PriorityEntity> queue = new PriorityBlockingQueue<PriorityEntity>();
+
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		executor.execute(new Runnable()
+
+		{
+
+			public void run()
+
+			{
+
+				int i = 0;
+
+				while (true)
+
+				{
+
+					queue.put(new PriorityEntity(random.nextInt(10), i++));
+
+					try
+
+					{
+
+						TimeUnit.MILLISECONDS.sleep(random.nextInt(1000));
+
+					}
+
+					catch (InterruptedException e)
+
+					{
+
+						log.error("", e);
+
+					}
+
+				}
+
+			}
+
+		});
+
+		executor.execute(new Runnable()
+
+		{
+
+			public void run()
+
+			{
+
+				while (true)
+
+				{
+
+					try
+
+					{
+
+						System.out.println("take-- " + queue.take() + " left:-- [" + queue.toString() + "]");
+
+						try
+
+						{
+
+							TimeUnit.MILLISECONDS.sleep(random.nextInt(3000));
+
+						}
+
+						catch (InterruptedException e)
+
+						{
+
+							log.error("", e);
+
+						}
+
+					}
+
+					catch (InterruptedException e)
+
+					{
+
+						log.error("", e);
+
+					}
+
+				}
+
+			}
+
+		});
+
+		try
+
+		{
+
+			TimeUnit.SECONDS.sleep(5);
+
+		}
+
+		catch (InterruptedException e)
+
+		{
+
+			log.error("", e);
+
+		}
+
+	}
+
+	static class PriorityEntity implements Comparable<PriorityEntity>
+
+	{
+
+		private static int count = 0;
+
+		private int id = count++;
+
+		private int priority;
+
+		private int index = 0;
+
+		public PriorityEntity(int _priority, int _index)
+
+		{
+
+			System.out.println("_priority : " + _priority);
+
+			this.priority = _priority;
+
+			this.index = _index;
+
+		}
+
+		public String toString()
+
+		{
+
+			return id + "# [index=" + index + " priority=" + priority + "]";
+
+		}
+
+		//数字小,优先级高
+
+		public int compareTo(PriorityEntity o)
+
+		{
+
+			return this.priority > o.priority ? 1 : this.priority < o.priority ? -1 : 0;
+
+		}
+
+	}
+}
