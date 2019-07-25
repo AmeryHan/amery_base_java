c51134eb60e8d85ec345f6636e0e9d4443546c9e
ameryhan <hanzhansheng@yodoo.net.cn>
2019-07-25 16:10:56 +0800

    修改spring quartz

--- ahan/src/main/java/amery/spring/quartz/CronRamStoreTest.java
+++ ahan/src/main/java/amery/spring/quartz/CronRamStoreTest.java
@@ -3,11 +3,11 @@ package amery.spring.quartz;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class JDBCJobStoreTest {
+public class CronRamStoreTest {
 
     public static void main(String[] args) {
         System.out.println("Test start.");
-        ApplicationContext context = new ClassPathXmlApplicationContext("amery/spring/quartz/JDBCJobStore.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("amery/spring/quartz/cronram.xml");
         System.out.print("Test end..\n");
     }
 
