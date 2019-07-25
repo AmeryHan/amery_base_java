72f3c9a129762bb55188f6f112930b5a9d4f76d0
ameryhan <hanzhansheng@yodoo.net.cn>
2019-07-25 16:40:35 +0800

    修改spring quartz

---
+++ spring-boot-quartz/src/main/java/quartz/config/BatchJobParam.java
@@ -0,0 +1,17 @@
+package quartz.config;
+
+/**
+ * @author ameryhan
+ * @date 2019/7/25 13:36
+ */
+public class BatchJobParam {
+    private String cron;
+
+    public String getCron() {
+        return cron;
+    }
+
+    public void setCron(String cron) {
+        this.cron = cron;
+    }
+}
