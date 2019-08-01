ee4e09b2ae4839393b9df2ea6266c565ca3dcbb3
ameryhan <hanzhansheng@yodoo.net.cn>
2019-08-01 15:40:05 +0800

    algorithm

---
+++ ahan/src/main/java/amery/algorithm/DuplicateProvider.java
@@ -0,0 +1,58 @@
+package amery.algorithm;
+
+/**
+ * @author ameryhan
+ * @date 2019/8/1 14:57
+ */
+public class DuplicateProvider {
+
+    public static boolean duplicate(int[] nums, int length, int[] duplication) {
+        if (nums == null || length <= 0)
+            return false;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    duplication[0] = nums[i];
+                    return true;
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return false;
+    }
+
+    public static boolean findFromMatrix(int target, int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        int rows = matrix.length, cols = matrix[0].length;
+        int r = 0, c = cols - 1;
+        // 从右上角开始
+        while (r <= rows - 1 && c >= 0) {
+            if (target == matrix[r][c])
+                return true;
+            else if (target > matrix[r][c])
+                r++;
+            else
+                c--;
+        }
+        return false;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        findFromMatrix(5, matrix);
+    }
+
+    public static void main1(String[] args) {
+        int[] nums = {2,3,1,0,2,5};
+        int[] duplication = {0};
+        duplicate(nums, 6, duplication);
+        System.out.println(duplication);
+    }
+}
