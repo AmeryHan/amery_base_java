893819b77fe76aaeb3ab1c82ee46573876cdd0ed
ameryhan <hanzhansheng@yodoo.net.cn>
2019-09-04 18:54:10 +0800

    add compare

---
+++ ahan/src/main/java/amery/interview/Sorter.java
@@ -0,0 +1,20 @@
+package amery.interview;
+
+import java.util.Comparator;
+
+public interface Sorter {
+    /**
+     * 排序
+     *
+     * @param list 待排序的数组
+     */
+    <T extends Comparable<T>> void sort(T[] list);
+
+    /**
+     * 排序
+     *
+     * @param list 待排序的数组
+     * @param comp 比较两个对象的比较器
+     */
+    <T> void sort(T[] list, Comparator<T> comp);
+}
